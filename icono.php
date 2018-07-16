3844181095c8f001f34aae2abe06f443b73e82ec
RavanH <ravanhagen@gmail.com>
2018-07-17 01:17:39 +0200

    Upgrade to stylesheet v1.3.2 hosted on KeyCDN

--- icono.php
+++ icono.php
@@ -54,7 +54,8 @@ SOFTWARE.
  */
 function icono_enqueue_scripts()
 {
-	wp_enqueue_style( 'icono-style', plugin_dir_url( __FILE__ ) . '/css/icono-master.min.css', array(), '1.3.1' );
+	//wp_enqueue_style( 'icono-style', plugin_dir_url( __FILE__ ) . '/css/icono-v1.3.2.min.css', array(), null );
+	wp_enqueue_style( 'icono-style', 'https://icono-49d6.kxcdn.com/icono.min.css', array(), null );
 
 	$custom_css = '.icon:before,.icon:after{box-sizing:content-box}';
 	wp_add_inline_style( 'icono-style', $custom_css );
